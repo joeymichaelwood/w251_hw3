09b98f2d80c7db35bf17c7e91c9bfc2719923823
Joey Wood <josephwood@ischool.berkeley.edu>
2022-03-28 15:52:49 -0700

    fix: name

--- cam_face_detector/cam.py
+++ cam_face_detector/cam.py
@@ -10,14 +10,14 @@ def on_connect_local(client, userdata, flags, rc):
     print("connected to local broker with rc: " + str(rc))
     client.subscribe(LOCAL_MQTT_TOPIC)
 
-# def on_message(client,userdata, msg):
-#   try:
-#     print("message received: ",str(msg.payload.decode("utf-8")))
-#     # if we wanted to re-publish this message, something like this should work
-#     # msg = msg.payload
-#     # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
-#   except:
-#     print("Unexpected error:", sys.exc_info()[0])
+# # def on_message(client,userdata, msg):
+# #   try:
+# #     print("message received: ",str(msg.payload.decode("utf-8")))
+# #     # if we wanted to re-publish this message, something like this should work
+# #     # msg = msg.payload
+# #     # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+# #   except:
+# #     print("Unexpected error:", sys.exc_info()[0])
 
 local_mqttclient = mqtt.Client()
 local_mqttclient.loop_start()
@@ -43,7 +43,7 @@ while(True):
         face = gray[y: y_height, x: x_width]
 
         _, pic = cv.imencode('.png', face)
-        remote_mqttclient.publish(LOCAL_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+        local_mqttclient.publish(LOCAL_MQTT_TOPIC, payload=msg, qos=0, retain=False)
 
     # Display the resulting frame
     cv.imshow('frame', gray)
